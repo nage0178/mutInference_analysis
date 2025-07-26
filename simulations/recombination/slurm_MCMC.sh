5f4bf18532827551cf0a000ee275f5af8c101a78
Anna Nagel <aanagel@ucdavis.edu>
2025-07-26 11:16:43 -0700

    Adding slurm files

---
+++ simulations/recombination/slurm_MCMC.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --partition=med2
+#SBATCH --account=brannalagrp
+#SBATCH --job-name=MCMCRecombHigh
+#SBATCH --mail-user=aanagel@ucdavis.edu
+#SBATCH --mail-type=ALL
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --array=61-120
+#SBATCH --cpus-per-task=16
+#SBATCH --time=32-0:00:00
+#SBATCH --output=log/%A.%a.out
+
+
+num=$((SLURM_ARRAY_TASK_ID))
+dir=$(head -$num MCMCdir| tail -n 1 )
+
+
+cd $dir
+
+	~/bpp/src/bpp --cfile inference.ctl --theta_mode 3 &> output &
+wait;
---
+++ simulations/recombination/slurm_argweaver2.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#SBATCH --partition=high2
+#SBATCH --account=brannalagrp
+#SBATCH --job-name=argweaver
+#SBATCH --mail-user=aanagel@ucdavis.edu
+#SBATCH --mail-type=ALL
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --array=1-600
+#SBATCH --time=20-0:00:00
+#SBATCH --mem=2G  
+#SBATCH --output=log/%A.%a.out
+
+
+#Only doing medium to start
+num=$((SLURM_ARRAY_TASK_ID))
+dir=$(awk -v i=$num '{print $i}' locusDir )
+
+
+
+cd $dir
+
+done=$(tail -n 1 arg2.log | grep FINISH)
+if [[ -z $done ]]
+then
+	#echo $dir >> ../../../argNotDone2
+	../../../argWeave.sh 2 &> outArgWeaver2
+fi
+
+wait; 
+
